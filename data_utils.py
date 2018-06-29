9a2063b444396318c4b575145bda62b68aa97a68
MarsPain <1434579002@qq.com>
2018-06-29 10:46:58 +0800

    完成日期和比例单位的归一化

--- data_utils.py
+++ data_utils.py
@@ -20,18 +20,28 @@ def read_data(dirname, dirname_txt):
             f = f.read()
             s = h2t.handle(f)   #读取为txt格式的string
             s = s.replace(" ", ""); s = s.replace("\n", "") #去除空格和换行符
-            #对日期进行归一化处理
-            pattern = re.compile(r"\d{4}"+"年"+r"\d{1,2}"+"月"+r"\d{1,2}"+"日")
-            def date_change(matchobj): #对匹配到的日期进行修改的函数
-                matchobj = re.sub("年|月", "-", matchobj[0])
-                matchobj = re.sub("日", "", matchobj)
-                return matchobj
-            s = re.sub(pattern, date_change, s)
-            # print(filename,s)
+            s = unit_norm(s)    #对日期和比例等单位进行归一化处理
             count += 1
             if count>100: break
             with open(filename_txt, "w", encoding="utf-8") as f_txt:
                 f_txt.write(s)
+
+def unit_norm(s):
+    # s = "他的生日是2016年12月12日，他在2017年8月7日至9月10日去大学了，有50%的学生"  #测试pattern
+    pattern = re.compile(r"(\d{4}年\d{1,2}月\d{1,2}日至\d{1,2}月\d{1,2}日)|(\d{4}年\d{1,2}月\d{1,2}日)|(\d+%)")
+    def replace(matchobj):
+        if matchobj[0][-1] == "%":
+            value = 0.01*int(matchobj[0][:-1])
+            matchobj = re.sub(matchobj[0], str(value), matchobj[0])
+        else:
+            year = matchobj[0][:4]
+            matchobj = re.sub("至", "至"+year+"年", matchobj[0])
+            matchobj = re.sub("年|月", "-", matchobj)
+            matchobj = re.sub("日", "", matchobj)
+        return matchobj
+    s = re.sub(pattern, replace, s)
+    return s
+
 
 if __name__ == "__main__":
     read_data(dirname_zengjianchi, dirname_txt_zengjianchi)
--- test.py
+++ test.py
@@ -2,14 +2,63 @@
 # print(a[2:-2])
 
 import re
-pattern = re.compile(r"\d{4}"+"年"+r"\d{1,2}"+"月"+r"\d{1,2}"+"日")
-s = "他的生日是2016年12月12日，他在2017年8月7日去大学了"
-print(re.findall(pattern,s))
-def test(matchobj):
+# pattern = re.compile(r"\d{4}"+"年"+r"\d{1,2}"+"月"+r"\d{1,2}"+"日")
+# s = "他的生日是2016年12月12日，他在2017年8月7日去大学了"
+# print(re.findall(pattern,s))
+# def test(matchobj):
+#     print(1)
+#     print(type(matchobj[0]))
+#     matchobj = re.sub("年|月", "-", matchobj[0])
+#     matchobj = re.sub("日", "", matchobj)
+#     return matchobj
+# s = re.sub(pattern, test, s)
+# print(s)
+
+# pattern2 = re.compile(r"(\d{4}年\d{1,2}月\d{1,2}日至\d{1,2}月\d{1,2}日)")
+# s = "他的生日是2016年12月12日，他在2017年8月7日至9月10日去大学了"
+# # print(re.findall(pattern,s))
+# def test2(matchobj):
+#     print(1)
+#     print(type(matchobj[0]))
+#     year = matchobj[0][:4]
+#     matchobj = re.sub("至", "至"+year+"年", matchobj[0])
+#     matchobj = re.sub("年|月", "-", matchobj)
+#     matchobj = re.sub("日", "", matchobj)
+#     return matchobj
+# s = re.sub(pattern2, test2, s)
+# print(s)
+
+# pattern3 = re.compile(r"(\d+%)")
+# s = "我50%"
+# def test3(matchobj):
+#     print("matchobj", matchobj[0])
+#     value = 0.01*int(matchobj[0][:-1])
+#     print(value)
+#     matchobj = re.sub(matchobj[0], str(value), matchobj[0])
+#     return matchobj
+# s = re.sub(pattern3, test3, s)
+# print(s)
+
+# s = "50%"
+# s = int(s[:-1])
+# print(s)
+
+pattern4 = re.compile(r"(\d{4}年\d{1,2}月\d{1,2}日至\d{1,2}月\d{1,2}日)|(\d{4}年\d{1,2}月\d{1,2}日)|(\d+%)")
+s = "他的生日是2016年12月12日，他在2017年8月7日至9月10日去大学了，有50%的学生"
+# print(re.findall(pattern,s))
+def test4(matchobj):
     print(1)
     print(type(matchobj[0]))
-    matchobj = re.sub("年|月", "-", matchobj[0])
-    matchobj = re.sub("日", "", matchobj)
+    if matchobj[0][-1] == "%":
+        value = 0.01*int(matchobj[0][:-1])
+        print(value)
+        matchobj = re.sub(matchobj[0], str(value), matchobj[0])
+        return matchobj
+    else:
+        year = matchobj[0][:4]
+        matchobj = re.sub("至", "至"+year+"年", matchobj[0])
+        matchobj = re.sub("年|月", "-", matchobj)
+        matchobj = re.sub("日", "", matchobj)
     return matchobj
-s = re.sub(pattern, test, s)
+s = re.sub(pattern4, test4, s)
 print(s)
