26e75a4f89a9648a68382e60457e277391145186
MarsPain <1434579002@qq.com>
2018-06-30 11:10:42 +0800

    将经过处理后的txt文件加工成命名实体识别程序ChineseNER能处理的格式，并根据6:2:2的比例输出到相应的训练集、验证集、测试集文件中。

--- data_utils.py
+++ data_utils.py
@@ -8,6 +8,9 @@ dirname_hetong = "data/round1_train_20180518/重大合同/html"
 dirname_txt_zengjianchi = "data/round1_train_20180518/增减持/txt"
 dirname_txt_dingzeng = "data/round1_train_20180518/定增/txt"
 dirname_txt_hetong = "data/round1_train_20180518/重大合同/txt"
+dirname_txt2_zengjianchi = "data/round1_train_20180518/增减持/txt2"
+dirname_txt2_dingzeng = "data/round1_train_20180518/定增/txt2"
+dirname_txt2_hetong = "data/round1_train_20180518/重大合同/txt2"
 
 def read_data(dirname, dirname_txt):
     h2t = html2text.HTML2Text() #初始化html2text工具
@@ -23,12 +26,40 @@ def read_data(dirname, dirname_txt):
             s = h2t.handle(f)   #读取为txt格式的string
             s = s.replace(" ", ""); s = s.replace("\n", "") #去除空格和换行符
             s = unit_norm(s)    #对日期和比例等单位进行归一化处理
-            s = announce_id + s
+            s = announce_id + s #将公告id(announce_id)加入每个相应文件的头部
             # print(s)
             count += 1
             # if count>10: break
             with open(filename_txt, "w", encoding="utf-8") as f_txt:
                 f_txt.write(s)
+
+def process_data(dirname):
+    announce_train = "data/round1_train_20180518/增减持/announce.train"
+    announce_dev = "data/round1_train_20180518/增减持/announce.dev"
+    announce_test = "data/round1_train_20180518/增减持/announce.test"
+    all_filenames = os.listdir(dirname)
+    # print(all_filenames)
+    num_filenames = len(all_filenames)
+    # num_filenames = 20
+    count = 0
+    announce_txt = ""
+    for filename in all_filenames:
+        filename = os.path.join(dirname, filename)
+        count += 1
+        with open(filename, 'r', encoding="utf-8") as f:
+            announce_txt = announce_txt + f.read() + "\n"
+            # print(announce_txt)
+            if count==6*(num_filenames//10):
+                with open(announce_train, 'w', encoding="utf-8") as train:
+                    train.write(announce_txt)
+                    announce_txt = ""
+            elif count==8*(num_filenames//10):
+                with open(announce_dev, 'w', encoding="utf-8") as dev:
+                    dev.write(announce_txt)
+                    announce_txt = ""
+            elif count==num_filenames:
+                with open(announce_test, 'w', encoding="utf-8") as test:
+                    test.write(announce_txt)
 
 def unit_norm(s):
     # s = "他的生日是2016年12月12日，他在2017年8月7日至9月10日去大学了，有50%的学生"  #测试pattern
@@ -48,4 +79,5 @@ def unit_norm(s):
 
 
 if __name__ == "__main__":
-    read_data(dirname_zengjianchi, dirname_txt_zengjianchi)
+    # read_data(dirname_zengjianchi, dirname_txt_zengjianchi)
+    process_data(dirname_txt2_zengjianchi)
